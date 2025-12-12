bc5edd797dc1720b3479b395b3f82bda00cc79b9
bree <bmbwb5@umsystem.edu>
2025-12-11 23:24:20 -0600

    implemented everything and seeing if CI worked?

--- src/app.py
+++ src/app.py
@@ -1,5 +1,62 @@
+import math
+
 def add (a, b):
+    if (not isinstance(a, (int, float))) or (not isinstance(b, (int, float))):
+        return "Invalid parameter passed! Not a number"
     return a+b
 
 def sub (a, b):
+    if (not isinstance(a, (int, float))) or (not isinstance(b, (int, float))):
+        return "Invalid parameter passed! Not a number"
     return a-b
+
+def mult (a, b):
+    if (not isinstance(a, (int, float))) or (not isinstance(b, (int, float))):
+        return "Invalid parameter passed! Not a number"
+    return a*b
+
+def div (a, b):
+    if (not isinstance(a, (int, float))) or (not isinstance(b, (int, float))):
+        return "Invalid parameter passed! Not a number"
+    try:
+        x = a/b
+        return x
+    except ZeroDivisionError:
+        return "Invalid: Divide by zero!"
+
+# Advanced Operations
+def log (a):
+    if not isinstance(a, (int, float)):
+        return "Invalid parameter passed! Not a number"
+    if a == 0:
+        return "Undefined!"
+    return math.log(a)
+
+def square (a, b=2):
+    if (not isinstance(a, (int, float))) or (not isinstance(b, (int, float))):
+        return "Invalid parameter passed! Not a number"
+    return a**b
+
+def sin (a):
+    if not isinstance(a, (int, float)):
+        return "Invalid parameter passed! Not a number"
+    return math.sin(a)
+
+def cos (a):
+    if not isinstance(a, (int, float)):
+        return "Invalid parameter passed! Not a number"
+    return math.cos(a)
+
+def square_root (a):
+    if not isinstance(a, (int, float)):
+        return "Invalid parameter passed! Not a number"
+    if a < 0:
+        return "Domain Error!"
+    return math.sqrt(a)
+
+def percentage(num, percent):
+    if (not isinstance(num, (int, float))) or (not isinstance(percent, (int, float))):
+        return "Invalid parameter passed! Not a number"
+    if percent < 0:
+        return "Invalid: percentage is negative!"
+    return num*(percent/100)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,99 @@
 import sys
 from pathlib import Path
+import math
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 
-from app import add
+import app as a
 
 def test_add():
-    assert add(5, 6) == 11
+    assert a.add(5, 6) == 11
 
 def test_add2():
-    assert add(5, 6) != 10
+    assert a.add(5, 6) != 10
+
+def test_add3():
+    assert a.add("a", "b") == "Invalid parameter passed! Not a number"
+
+def test_sub():
+    assert a.sub(5, 6) == -1
+
+def test_sub2():
+    assert a.sub(5, 6) != 11
+
+def test_sub3():
+    assert a.sub("a", "b") == "Invalid parameter passed! Not a number"
+
+def test_mult():
+    asser a.mult(5, 6) == 30
+
+def test_mult2():
+    assert a.mult(-5, 6) == -30
+
+def test_mult3():
+    assert a.mult("a", "b") == "Invalid parameter passed! Not a number"
+
+def test_div():
+    assert a.div(10, 5) == 2
+
+def test_div2():
+    assert a.div(10, 0) == "Invalid: Divide by zero!"
+
+def test_div3():
+    assert a.div("a", "b") == "Invalid parameter passed! Not a number"
+
+def test_log():
+    assert a.log(1) == 0
+
+def test_log2():
+    assert a.log(0) == "Undefined!"
+
+def test_log3():
+    assert a.log("a") == "Invalid parameter passed! Not a number"
+
+def test_square():
+    assert a.square(1, 3) == 3
+
+def test_square2():
+    assert a.square(2) == 4
+
+def test_square3():
+    assert a.square("a") == "Invalid parameter passed! Not a number"
+
+def test_sin():
+    assert a.sin(0) == 0
+
+def test_sin2():
+    assert a.sin(math.radians(90)) == 1
+
+def test_sin3():
+    assert a.sin("a") == "Invalid parameter passed! Not a number"
+
+def test_cos():
+    assert a.cos(0) == 1
+
+def test_cos2():
+    assert a.cos(math.radians(90)) == 0
+
+def test_cos3():
+    assert a.cos("a") == "Invalid parameter passed! Not a number"
+
+def test_square_root():
+    assert a.square_root(4) == 2
+
+def test_square_root2():
+    assert a.square_root(-4) == "Domain Error!"
+
+def test_square_root3():
+    assert a.square_root("a") == "Invalid parameter passed! Not a number"
+
+def test_percentage():
+    assert a.percentage(1, 100) == 1
+
+def test_percentage2():
+    assert a.percentage(1, -10) == "Invalid: percentage is negative!"
+
+def test_percentage3():
+    assert a.percentage("a, b") == "Invalid parameter passed! Not a number"
