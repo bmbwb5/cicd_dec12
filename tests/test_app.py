d8c8a40af6ccf295e9f5a84437a102d95f9809e0
bree <bmbwb5@umsystem.edu>
2025-12-12 18:29:54 -0600

    fixing code to follow good logic practices

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 import math
-import random
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
@@ -16,7 +15,11 @@ def test_add2():
     assert a.add(5, 6) != 10
 
 def test_add3():
-    assert a.add("a", "b") == "Invalid parameter passed! Not a number"
+    try:
+        a.add("a", "b")
+        assert False
+    except ValueError:
+        assert True
 
 def test_sub():
     assert a.sub(5, 6) == -1
@@ -25,7 +28,11 @@ def test_sub2():
     assert a.sub(5, 6) != 11
 
 def test_sub3():
-    assert a.sub("a", "b") == "Invalid parameter passed! Not a number"
+    try:
+        a.sub("a", "b")
+        assert False
+    except ValueError:
+        assert True
 
 def test_mult():
     assert a.mult(5, 6) == 30
@@ -34,25 +41,45 @@ def test_mult2():
     assert a.mult(-5, 6) == -30
 
 def test_mult3():
-    assert a.mult("a", "b") == "Invalid parameter passed! Not a number"
+    try:
+        a.mult("a", "b")
+        assert False
+    except ValueError:
+        assert True
 
 def test_div():
     assert a.div(10, 5) == 2
 
 def test_div2():
-    assert a.div(10, 0) == "Invalid: Divide by zero!"
+    try:
+        a.div(10, 0)
+        assert False
+    except ZeroDivisionError:
+        assert True
 
 def test_div3():
-    assert a.div("a", "b") == "Invalid parameter passed! Not a number"
+    try:
+        a.div("a", "b")
+        assert False
+    except ValueError:
+        assert True
 
 def test_log():
     assert a.log(1) == 0
 
 def test_log2():
-    assert a.log(0) == "Undefined!"
+    try:
+        a.log(0)
+        assert False
+    except ValueError:
+        assert True
 
 def test_log3():
-    assert a.log("a") == "Invalid parameter passed! Not a number"
+    try:
+        a.log("a")
+        assert False
+    except ValueError:
+        assert True
 
 def test_square():
     assert a.square(1, 3) == 1
@@ -61,7 +88,11 @@ def test_square2():
     assert a.square(2) == 4
 
 def test_square3():
-    assert a.square("a") == "Invalid parameter passed! Not a number"
+    try:
+        a.square("a")
+        assert False
+    except ValueError:
+        assert True
 
 def test_sin():
     assert math.isclose(a.sin(0), 0)
@@ -70,7 +101,11 @@ def test_sin2():
     assert math.isclose(a.sin(math.radians(90)), 1)
 
 def test_sin3():
-    assert a.sin("a") == "Invalid parameter passed! Not a number"
+    try:
+        a.sin("a")
+        assert False
+    except ValueError:
+        assert True
 
 def test_cos():
     assert math.isclose(a.cos(0), 1)
@@ -79,22 +114,42 @@ def test_cos2():
     assert math.isclose(a.cos(math.radians(180)), -1)
 
 def test_cos3():
-    assert a.cos("a") == "Invalid parameter passed! Not a number"
+    try:
+        a.cos("a")
+        assert False
+    except ValueError:
+        assert True
 
 def test_square_root():
     assert a.square_root(4) == 2
 
 def test_square_root2():
-    assert a.square_root(-4) == "Domain Error!"
+    try:
+        a.square_root(-4)
+        assert False
+    except ValueError:
+        assert True
 
 def test_square_root3():
-    assert a.square_root("a") == "Invalid parameter passed! Not a number"
+    try:
+        a.square_root("a")
+        assert False
+    except ValueError:
+        assert True
 
 def test_percentage():
     assert a.percentage(1, 100) == 1
 
 def test_percentage2():
-    assert a.percentage(1, -10) == "Invalid: percentage is negative!"
+    try:
+        a.percentage(1, -10)
+        assert False
+    except ValueError:
+        assert True
 
 def test_percentage3():
-    assert a.percentage("a", "b") == "Invalid parameter passed! Not a number"
+    try:
+        a.percentage("a", "b")
+        assert False
+    except ValueError:
+        assert True
